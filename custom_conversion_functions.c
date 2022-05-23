df8549f5d2a62eb57e17ece47ec98d7481fee1d5
ThankGod88 <enemuothankgod@yahoo.com>
2022-05-22 18:54:37 -0700

    custom_conversion_functions.c

---
+++ custom_conversion_functions.c
@@ -0,0 +1,60 @@
+#include <stdlib.h>
+#include <unistd.h>
+
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
+
+
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (*s++)
+	{
+		len++;
+	}
+	return (len);
+}
+/**
+ *_puts - print string
+ *Description: print some string
+ *@str: pointer char
+ *Return: Nothing
+ */
+void _puts(char *str)
+{
+	int i;
+
+	for (i = 0; i < _strlen(str); i++)
+	{
+		_putchar(str[i]);
+	}
+}
+
+
+char *convert_to(char representation[], unsigned int num, int base)
+{
+	char *ptr;
+	static char buffer[128];
+	int mod = 0;
+
+	ptr = &buffer[127];
+	*ptr = '\0';
+
+	do {
+		mod = num % base;
+		*--ptr = representation[mod];
+		num /= base;
+	} while (num != 0);
+	return (ptr);
+}
